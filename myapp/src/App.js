5ab9321e1cb88c076386e7af63ccc21eb514936c
mr-fox93 <lisieckikamil93@gmail.com>
2023-03-27 12:11:41 +0200

    useState,useEffect

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,14 +1,54 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <h1>Hello Word</h1>
-      </React.Fragment>
-    );
-  }
-}
+const KEY = "0ee0370fa3e7cf6b96a74c88d350ddb7";
+
+const App = () => {
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState("");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    if (!city) {
+      return;
+    }
+
+    const URL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${KEY}`;
+
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.cod === "404") {
+          setError(data.message);
+          setWeatherData(null);
+        } else {
+          setWeatherData(data);
+          setError("");
+        }
+      });
+  }, [city]);
+
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <input
+        type="text"
+        value={city}
+        onChange={(event) => setCity(event.target.value)}
+      />
+      {error ? (
+        <div>{error}</div>
+      ) : weatherData ? (
+        <div>
+          <p>Weather: {weatherData.weather[0].main}</p>
+          <p>Temperature: {weatherData.main.temp}°C</p>
+          <p>Wind Speed: {weatherData.wind.speed}m/s</p>
+        </div>
+      ) : (
+        <div>No weather data available</div>
+      )}
+    </div>
+  );
+};
 
 export default App;
